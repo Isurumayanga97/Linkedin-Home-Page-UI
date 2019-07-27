3bf6cddc7c93c23f5c9dde29b6a5130d4626b1f2
isuru <umayangaisuru97@gmail.com>
2019-07-27 19:29:12 +0530

    1.1.0

--- mod1/js/myjs.js
+++ mod1/js/myjs.js
@@ -1,5 +1,12 @@
-$("#btnPost").click(
+$("#messaging").click(
     function () {
-        $("#newpop").css("display","inline")
+        $("#newpop").css("display", "inline")
     }
-)
+);
+$("#messagingafter").click(
+    function () {
+        $("#newpop").css("display", "none")
+    }
+);
+
+

343d11c157e959397a83872ff9fef1a60c89296b
isuru <umayangaisuru97@gmail.com>
2019-07-28 13:12:02 +0530

    -m "1.3.0"

--- mod1/js/myjs.js
+++ mod1/js/myjs.js
@@ -8,5 +8,26 @@ $("#messagingafter").click(
         $("#newpop").css("display", "none")
     }
 );
+var modal = document.getElementById("mypost");
+var btn = document.getElementById("btnstartpos");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+span.onclick = function () {
+    modal.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/////////////////////////////////////////////////////
+$("#exampleFormControlTextarea1").click(function () {
+
+    $("#btnpost").attr("disabled", false);
+});
+
 
 
